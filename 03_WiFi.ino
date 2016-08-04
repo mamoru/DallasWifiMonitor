7658ae00deaf67edaba1698d7237c24e471ee678
webbes <websware@hotmail.com>
2016-08-04 13:28:58 +0200

    Updated comment

--- 03_WiFi.ino
+++ 03_WiFi.ino
@@ -3,7 +3,7 @@ void ConnectWiFi() {
 	wifiManager.setTimeout(180);
 	if (!wifiManager.autoConnect()) {
 		// in case of a power outage where we cannot connect to the WiFi temporarely
-		// we don't want to be stuck in the config portal forever... if 
+		// we don't want to be stuck in the config portal forever...
 		ESP.reset();
 	}
 }
